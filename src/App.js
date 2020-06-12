3597cda0d5e9ec170e2f4263bbb9c0cfae4a802a
Alexander Repeta <alexander.repeta@gmail.com>
2020-06-12 19:13:49 +0300

    Пофиксили ошибки, но оставили предупреждения

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import Logo from "./components/Logo";
-import PaintingList from "./components/PaintingList";
-import Panel from "./components/Panel";
-import paintings from "./paintings.json";
+import React from 'react';
+import Logo from './components/Logo';
+import PaintingList from './components/PaintingList';
+import Panel from './components/Panel';
+import paintings from './paintings.json';
 
 const colorPickerOptions = [
-  { label: "red", color: "#F44336" },
-  { label: "green", color: "#4CAF50" },
-  { label: "blue", color: "#2196F3" },
-  { label: "grey", color: "#607D8B" },
-  { label: "pink", color: "#E91E63" },
-  { label: "indigo", color: "#3F51B5" },
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
 ];
 
 const App = () => {
